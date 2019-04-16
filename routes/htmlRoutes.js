f55238ae25ab812b62322fbed69b98c1dd67cfcf
Ashell Fernando <ashellz2010@gmail.com>
2019-04-16 14:13:17 -0400

    fixed route issue

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,9 +3,6 @@ const path = require('path');
 // this file basically directs which file to show depending on the route
 module.exports = app => {
   //this shows to any route that's not described below
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, `../public/home.html`));
-  });
 
   //this shows the tables html when localhost:3000/tables gets hit
   app.get('/tables', (req, res) => {
@@ -14,5 +11,8 @@ module.exports = app => {
   //this shows the current tables html when localhost:3000/reserve gets hit
   app.get('/reserve', (req, res) => {
     res.sendFile(path.join(__dirname, `../public/reserve.html`));
+  });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, `../public/home.html`));
   });
 };
